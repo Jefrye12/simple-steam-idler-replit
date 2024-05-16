b559ba3f207165036668638e220c1f43d0048929
Jefrye12 <126428297+Jefrye12@users.noreply.github.com>
2024-05-16 13:53:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [700330];  // Enter here AppIDs of the needed games
+var games = [700330, 730, 1782380];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
